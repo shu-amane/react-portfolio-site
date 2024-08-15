e7c85ac5e5a764dacf752fd2502403475680c7a8
root <amane.shu0328@gmail.com>
2024-08-15 12:21:37 +0000

    fix portfolio

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+export function Contact() {
+    return (
+        <div id="contact" className="contact">
+            <div className="container">
+                <h2>Contact To Me</h2>
+                <p className="description">お仕事のご相談・ご依頼など下記よりご連絡ください。</p>
+                <a href="https://x.com/basrossDev" className="btn">Contact Us</a>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import CoverImage from "../images/cover-image.jpg"
+import ProfileImage from "../images/profile-image.jpg"
+import { FaTwitter, FaGithub } from "react-icons/fa"
+
+export function Header() {
+    return (
+        <header className="main-cover" style={{backgroundImage: `url(${CoverImage})`}}>
+            <div className="overlay"></div>
+            <div className="container">
+                <div className="display-table">
+                    <div className="display-table-contents">
+                        <div className="profile-thumb" style={{backgroundImage: `url(${ProfileImage})`}}></div>
+                        <h1 className="title-text">上田 周</h1>
+                        <h3 className="title-text">Engineer</h3>
+                        <h1 className="social-icons"></h1>
+                        <li className="icon-link">
+                            <a href="https://x.com/basrossDev">
+                            <FaTwitter color="white" size="2rem"/>
+                            </a>
+                        </li>
+                        <li className="icon-link">
+                            <a href="https://github.com/shu-amane">
+                                <FaGithub color="white" size="2rem"/>
+                            </a>
+                        </li>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
